5a983ce039addea3d17602c7ebae75e8054d3e46
azizzakiryarov <aziz.zakiryarov@technia.com>
2020-01-21 15:30:29 +0100

    Created new Security Service

--- src/main/java/se/azza/teamservice/repository/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package se.azza.teamservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import se.azza.teamservice.model.Team;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {
-	
-}
